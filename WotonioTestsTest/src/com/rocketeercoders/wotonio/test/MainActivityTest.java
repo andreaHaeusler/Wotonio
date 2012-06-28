e3a2fe820006c3226a39a4b0f7c73e699ce29600
Kurt Haeusler <kurt.haeusler@gmail.com>
2012-06-28 21:48:02 +0200

    Added a test project and added a basic Activity Test.

---
+++ WotonioTestsTest/src/com/rocketeercoders/wotonio/test/MainActivityTest.java
@@ -0,0 +1,43 @@
+package com.rocketeercoders.wotonio.test;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.rocketeercoders.wotonio.MainActivity;
+
+public class MainActivityTest extends
+		android.test.ActivityInstrumentationTestCase2<MainActivity> {
+
+	private MainActivity activity;
+	private TextView tv;
+	private Button b;
+	private String initialText;
+
+	public MainActivityTest() {
+		super("com.rocketeercoders.wotonio.MainActivity", MainActivity.class);
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		activity = getActivity();
+		tv = (TextView) activity
+				.findViewById(com.rocketeercoders.wotonio.R.id.tvMessages);
+		b = (Button) activity
+				.findViewById(com.rocketeercoders.wotonio.R.id.bLetsGetDrinking);
+		initialText = activity
+				.getString(com.rocketeercoders.wotonio.R.string.not_yet_clicked);
+
+	}
+
+	public void testPreconditions() {
+		assertNotNull(tv);
+		assertNotNull(b);
+
+	}
+
+	public void testText() {
+		assertEquals(initialText, (String) tv.getText());
+	}
+
+}
