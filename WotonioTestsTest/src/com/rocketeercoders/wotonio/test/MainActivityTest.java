4e93c2a318e076338833d18b8166b0399db45198
Kurt Haeusler <kurt.haeusler@gmail.com>
2012-06-28 22:48:42 +0200

    Added another test for the button, and moved some strings into resources.

--- WotonioTestsTest/src/com/rocketeercoders/wotonio/test/MainActivityTest.java
+++ WotonioTestsTest/src/com/rocketeercoders/wotonio/test/MainActivityTest.java
@@ -1,5 +1,7 @@
 package com.rocketeercoders.wotonio.test;
 
+import android.os.Handler;
+import android.test.UiThreadTest;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -11,10 +13,12 @@ public class MainActivityTest extends
 	private MainActivity activity;
 	private TextView tv;
 	private Button b;
-	private String initialText;
+	private String initialText, textFormat;
+	private Handler handler;
 
 	public MainActivityTest() {
 		super("com.rocketeercoders.wotonio.MainActivity", MainActivity.class);
+		handler = new Handler();
 	}
 
 	@Override
@@ -27,6 +31,8 @@ public class MainActivityTest extends
 				.findViewById(com.rocketeercoders.wotonio.R.id.bLetsGetDrinking);
 		initialText = activity
 				.getString(com.rocketeercoders.wotonio.R.string.not_yet_clicked);
+		textFormat = activity
+				.getString(com.rocketeercoders.wotonio.R.string.you_have_had);
 
 	}
 
@@ -38,6 +44,12 @@ public class MainActivityTest extends
 
 	public void testText() {
 		assertEquals(initialText, (String) tv.getText());
+	}
+
+	@UiThreadTest
+	public void testButtonPush() {
+		b.performClick();
+		assertEquals(String.format(textFormat, 1), (String) tv.getText());
 	}
 
 }
