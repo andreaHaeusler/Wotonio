54146185f76d2ddc6e1fc86368eede20ba3e5e00
Kurt Haeusler <kurt.haeusler@gmail.com>
2012-06-29 17:33:22 +0200

    Just some formatting

--- src/com/rocketeercoders/wotonio/DBClass.java
+++ src/com/rocketeercoders/wotonio/DBClass.java
@@ -6,8 +6,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class DBClass
-{
+public class DBClass {
 	private static final String DATABASE_NAME = "KeepingTrackOfWaterDrunk";
 	public static final int DATABASE_VERSION = 1;
 	private static final String DATABASE_TABLE = "waterTable";
@@ -17,17 +16,14 @@ public class DBClass
 	private Context buttonHasBeenPushed;
 	private SQLiteDatabase ourDatabase;
 
-	private static class DbHelper extends SQLiteOpenHelper
-	{
-		public DbHelper(Context context)
-		{
+	private static class DbHelper extends SQLiteOpenHelper {
+		public DbHelper(Context context) {
 			super(context, DATABASE_NAME, null, DATABASE_VERSION);
 			// TODO Auto-generated constructor stub
 		}
 
 		@Override
-		public void onCreate(SQLiteDatabase db)
-		{
+		public void onCreate(SQLiteDatabase db) {
 			// TODO Auto-generated method stub
 			db.execSQL("CREATE TABLE " + DATABASE_TABLE + " (" + KEY_COUNT
 					+ " INTEGER)");
@@ -35,8 +31,7 @@ public class DBClass
 		}
 
 		@Override
-		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
-		{
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 			// TODO Auto-generated method stub
 			db.execSQL("DROP TABLE IF EXISTS " + DATABASE_TABLE);
 			onCreate(db);
@@ -45,32 +40,27 @@ public class DBClass
 
 	}
 
-	public DBClass(Context c)
-	{
+	public DBClass(Context c) {
 		buttonHasBeenPushed = c;
 	}
 
-	public DBClass openDatabse()
-	{
+	public DBClass openDatabse() {
 		ourHelper = new DbHelper(buttonHasBeenPushed);
 		ourDatabase = ourHelper.getWritableDatabase();
 		return this;
 	}
 
-	public void closeDatabase()
-	{
+	public void closeDatabase() {
 		ourHelper.close();
 	}
 
-	public int getCount()
-	{
+	public int getCount() {
 		int dbValue = 0;
 		String[] columns = new String[] { KEY_COUNT };
 		Cursor c = ourDatabase.query(DATABASE_TABLE, columns, null, null, null,
 				null, null);
 
-		for (c.moveToFirst(); !c.isAfterLast(); c.moveToNext())
-		{
+		for (c.moveToFirst(); !c.isAfterLast(); c.moveToNext()) {
 			dbValue = c.getInt(0);
 		}
 
@@ -78,8 +68,7 @@ public class DBClass
 		return dbValue;
 	}
 
-	public void addAGlassOfWater(int waterCounter)
-	{
+	public void addAGlassOfWater(int waterCounter) {
 		// TODO Auto-generated method stub
 		ContentValues cv = new ContentValues();
 		cv.put(KEY_COUNT, waterCounter);
