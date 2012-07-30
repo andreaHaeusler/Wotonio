aa4617a1f41b6f9b83d6d2cfca03c1f932775a14
Andrea Haeusler <andrea.haeusler@googlemail.com>
2012-07-30 14:10:54 +0200

    Have changed the background colour

--- src/main/java/com/rocketeercoders/wotonio/MainActivity.java
+++ src/main/java/com/rocketeercoders/wotonio/MainActivity.java
@@ -3,12 +3,15 @@ package com.rocketeercoders.wotonio;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.sqlite.SQLiteException;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener {
@@ -16,14 +19,20 @@ public class MainActivity extends Activity implements OnClickListener {
 	private Button b, bResetDB, bConsumed;
 	private int toastTextDuration = Toast.LENGTH_SHORT;
 
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		//this will get rid of the inevitable banding effect (stripes in background)...
+		getWindow().setFormat(PixelFormat.RGBA_8888);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_DITHER);
 		setContentView(R.layout.activity_main);
 
 		// this buttons should take user to a new page where they can select the
 		// drink that they are going to drink
 		b = (Button) findViewById(R.id.bLetsGetDrinking);
+		
+		
 		// the reset button is only for our use, not for the user
 		bResetDB = (Button) findViewById(R.id.bResetDB);
 		// this button should take user to graph indicating consumtion
